54c5d88af7e26da6c1e2b440d7c1f5789b035558
acer <wongsatorn1501003@gmail.com>
2019-10-17 20:12:16 +0700

    6

--- app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
@@ -2,17 +2,19 @@ package com.example.youtubeplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener {
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+        /*setContentView(R.layout.activity_youtube)
        //val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
         val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
         setContentView(layout)
@@ -20,15 +22,23 @@ class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListene
        val button1 = Button(this)
         button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
        button1.text = "Button added"
-       layout.addView(button1)
+       layout.addView(button1)*/
+
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
     }
 
-    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean
     ) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?
     ) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
