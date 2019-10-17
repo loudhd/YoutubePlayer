f594d98ad8f51508509b760b8d8f20a05cc67881
acer <wongsatorn1501003@gmail.com>
2019-10-17 19:59:47 +0700

    3

--- app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
@@ -3,11 +3,23 @@ package com.example.youtubeplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
-class YoutubeActivity : YouTubeBaseActivity() {
+class YoutubeActivity : YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
